899a29578d53e349309d038567012f1bc7692869
temple111 <templealvaro@gmail.com>
2022-04-11 20:04:51 +0200

    Video 6

---
+++ sql/setup.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXITS contacts_app;
+
+CREATE DATABASE contacts_app;
+USE contacts_app;
+CREATE TABLE contacts(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    phone_number VARCHAR(255)
+);
+
+INSERT INTO contacts (name, phone_number) VALUES ("Pepe", "12331231");
